3ff26772a5fff92881d22ec4181360406d102efd
renato salvatori <renato.s@cost.it>
2021-12-08 21:35:42 +0100

    versione finale pre-authentication

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 export const useGetData = (url) => {
   const [data, setData] = useState();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(true);
+  debugger
 
   useEffect(() => {
     async function fetchData() {
@@ -22,4 +24,25 @@ export const useGetData = (url) => {
   }, [url])
 
   return {data, error, loading}
+}
+
+const fetcher = (url) =>
+    fetch(url).then(async res => {
+      const result = await res.json();
+
+      if (res.status !== 200) {
+        return Promise.reject(result);
+      } else {
+        return result;
+      }
+    });
+
+export const useGetPosts = () => {
+  const {data, error, ...rest} = useSWR('/api/v1/posts', fetcher);
+  return {data, error, loading: !data && !error, ...rest}
+}
+
+export const useGetPostById = (id) => {
+  const {data, error, ...rest} = useSWR(id ? `/api/v1/posts/${id}` : null,  fetcher);
+  return {data, error, loading: !data && !error, ...rest}
 }
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
+/*app.prepare().then(() => {
   const server = express();
 
   server.all('*', (req, res) => {
@@ -19,3 +19,4 @@ app.prepare().then(() => {
     console.log(`> Ready on port ${PORT}`)
   })
 })
+*/
